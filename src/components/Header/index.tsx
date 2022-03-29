ad3d89560301d7f430e47dafdf75fe0e54e67e43
tomvieir <wevertonbvieira@gmail.com>
2022-03-29 18:24:40 -0300

    Haader refatorado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,8 +4,12 @@ import { FiPlusSquare } from 'react-icons/fi';
 import { Container } from './styles';
 import Logo from '../../assets/logo.svg';
 
-export function Header() {
-  const { openModal } = this.props;
+interface HeaderProps {
+  openModal: () => void
+}
+
+export function Header({openModal}:HeaderProps) {
+  
 
   return (
     <Container>
